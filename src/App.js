30b9ad702ce255ed9ab2d2bd03c5f8fa1c5f4b42
TheYuvCo <yuval01958@gmail.com>
2022-11-18 15:50:25 +0200

    new one

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>lalalal</h1>
+      <h1>yuvalcohen</h1>
       yuval
     </div>
   );
 }
-
 export default App;
