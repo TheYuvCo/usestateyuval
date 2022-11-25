d6443cf5fccefa1274d9387cdfc858a6e2ea7d0a
TheYuvCo <yuval01958@gmail.com>
2022-11-25 11:53:06 +0200

    exc_3_max_number

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Counter from './counter';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [delta,setDelta] = useState(1)
+  const max = 200;
+
+
+  function handleDelta(event){
+    console.log(event);
+    setDelta(Number(event.target.value))
+  }
+
+
   return (
     <div className="App">
-      <Counter></Counter>
+      <input type = "number" value = {delta} onChange = {handleDelta}/>
+      <Counter  max = {max} delta = {delta}/>
+      <Counter  max = {max} delta = {delta}/>
     </div>
   );
 }
